168a4442fe9ae031a382ae39c4bc5b0cd744766b
vishalvatsalya7 <vishalvatsalya7@gmail.com>
2020-08-02 22:55:51 +0530

    added template

---
+++ mytemplate.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define ar array
+#define ff first
+#define vi vector<int>
+#define vll vector<ll>
+#define ss second
+#define all(x) x.begin(),x.end()
+#define maxiheap priority_queue<int>
+#define miniheap priority_queue<int, vector<int>, greater<int>>
+#define maxlheap priority_queue<ll>
+#define minlheap priority_queue<ll, vector<ll>, greater<ll>>
+using namespace std;
+ll binpow(ll a, ll b, ll m){a %= m;ll res = 1;while (b > 0){if (b & 1) res = res * a % m;a = a * a % m;b >>= 1;}return res;}
+/* End of template */
+
+int main(){
+    
+}
+
